7b92aa41cc2e600309e8bc6061762c50666962c4
admin <admin@admins-iMac.local>
2017-02-09 10:31:45 +0200

    First version with tables

--- HeadDive/ViewController.swift
+++ HeadDive/ViewController.swift
@@ -10,16 +10,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    private struct StoryBorad
+    {
+        static let ShowQuizSegue = "showQuiz"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
+        
+        
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+   
+    @IBAction func showQuiz(_ sender: UIButton) {
+        performSegue(withIdentifier: StoryBorad.ShowQuizSegue, sender: sender)
+
     }
 
+  
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == StoryBorad.ShowQuizSegue {
+            if let ivc = segue.destination.contentViewControler as? QuizViewController {
+//                let imageName = (sender as? UIButton)?.currentTitle
+//                ivc.imageURL = DemoURL.NASAImageNamed(imageName: imageName)
+                
+                ivc.title = "Backy"
+            }
+        }
+    }
 
 }
 
